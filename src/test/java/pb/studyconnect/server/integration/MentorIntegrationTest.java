0c28b5f37ff54e81aa4d3d112f39dcf5120c8acd
coffe <a.potapova8@g.nsu.ru>
2024-10-19 13:25:30 +0700

    fix tests

--- src/test/java/pb/studyconnect/server/integration/MentorIntegrationTest.java
+++ src/test/java/pb/studyconnect/server/integration/MentorIntegrationTest.java
@@ -1,31 +1,33 @@
 package pb.studyconnect.server.integration;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import io.restassured.RestAssured;
 
-import java.io.IOException;
-import java.util.Properties;
 
 public class MentorIntegrationTest extends BaseIntegrationTest {
 
     @Test
     public void createMentor() {
+        // Создание научного руководителя
         Response response = given().header("Content-Type", "application/json")
                 .and()
                 .body("""
-            {
-              "name": "Дмитрий Иртеров",
-              "email": "fat@nsu.ru",
-              "tgNickname": "@fatBrother",
-              "scientificInterests": ["ОСИ", "peer-to-peer", "CDM-16"],
-              "diplomaTopics": ["Системы оркестрации контейнеров бла бла бла"],
-              "department": "кафедра систем информатики"
-            }
-            """)
+                        {
+                          "name": "Дмитрий Иртеров",
+                          "email": "fat@nsu.ru",
+                          "tgNickname": "@fat_brother",
+                          "scientificInterests": ["ОСИ", "peer-to-peer", "CDM-16"],
+                          "department": "кафедра систем информатики",
+                          "diplomaTopics": [{
+                                "name": "Разработка модуля для системы оркестрации контейнеров",
+                                "summary": "надо че то сделать",
+                                "neededSkills": ["стрессоустойчивость"],
+                                "scientificField": "системы оркестрации контейнеров"
+                          }]
+                        }
+                        """)
                 .when()
                 .post("/profiles/mentors")
                 .then()
@@ -33,38 +35,25 @@ public class MentorIntegrationTest extends BaseIntegrationTest {
 
         Assertions.assertEquals(200, response.statusCode());
         Assertions.assertEquals("Дмитрий Иртеров", response.jsonPath().getString("name"));
-
-        response = given().header("Content-Type", "application/json")
-                .and()
-                .body("""
-            {
-              "name": "Абоба Сигмович",
-              "email": "aboba@gmail.ru",
-              "tgNickname": "@sigma",
-              "scientificInterests": ["CDM-16", "LLM","OS"],
-              "skills": ["Assembler","vim","linux"],
-              "initiativeTheme": "Генерация аssembler кода с использованием GPT"
-            }
-            """)
-                .when()
-                .post("/profiles/mentors")
-                .then()
-                .extract().response();
-
-        Assertions.assertNotEquals(200, response.statusCode());
     }
     @Test
     public void checkValidationMin3ElementsAtList() {
+        // Ошибка, тк должно быть как минимум 3 элемента в scientificInterests
         Response response = given().header("Content-Type", "application/json")
                 .and()
                 .body("""
             {
               "name": "Дмитрий Иртеров",
               "email": "fat@nsu.ru",
-              "tgNickname": "@fatBrother",
+              "tgNickname": "@fat_brother",
               "scientificInterests": ["ОСИ", "peer-to-peer"],
-              "diplomaTopics": ["Системы оркестрации контейнеров бла бла бла"],
-              "department": "кафедра систем информатики"
+              "department": "кафедра систем информатики",
+              "diplomaTopics": [{
+                    "name": "Разработка модуля для системы оркестрации контейнеров",
+                   "summary": "надо че то сделать",
+                   "neededSkills": ["стрессоустойчивость"],
+                   "scientificField": "системы оркестрации контейнеров"
+              }]
             }
             """)
                 .when()
@@ -78,34 +67,46 @@ public class MentorIntegrationTest extends BaseIntegrationTest {
         Response response = given().header("Content-Type", "application/json")
                 .and()
                 .body("""
-            {
-              "name": "Дмитрий Иртеров",
-              "email": "fatnsu.ru",
-              "tgNickname": "@fatBrother",
-              "scientificInterests": ["ОСИ", "peer-to-peer"],
-              "diplomaTopics": ["Системы оркестрации контейнеров бла бла бла"],
-              "department": "кафедра систем информатики"
-            }
-            """)
+                        {
+                          "name": "Дмитрий Иртеров",
+                          "email": "fat@nsuru",
+                          "tgNickname": "@fat_brother",
+                          "scientificInterests": ["ОСИ", "peer-to-peer"],
+                          "department": "кафедра систем информатики",
+                          "diplomaTopics": [{
+                                "name": "Разработка модуля для системы оркестрации контейнеров",
+                               "summary": "надо че то сделать",
+                               "neededSkills": ["стрессоустойчивость"],
+                               "scientificField": "системы оркестрации контейнеров"
+                          }]
+                        }
+                        """)
                 .when()
                 .post("/profiles/mentors")
                 .then()
                 .extract().response();
 
         Assertions.assertNotEquals(200, response.statusCode());
-
-        response = given().header("Content-Type", "application/json")
+    }
+    @Test
+    public void checkValidationEmail2() {
+        Response response = given().header("Content-Type", "application/json")
                 .and()
                 .body("""
-            {
-              "name": "Дмитрий Иртеров",
-              "email": "fat!nsu",
-              "tgNickname": "@fatBrother",
-              "scientificInterests": ["ОСИ", "peer-to-peer"],
-              "diplomaTopics": ["Системы оркестрации контейнеров бла бла бла"],
-              "department": "кафедра систем информатики"
-            }
-            """)
+                        {
+                          "name": "Дмитрий Иртеров",
+                          "email": "fat!nsu.ru",
+                          "tgNickname": "@fat_brother",
+                          "scientificInterests": ["ОСИ", "peer-to-peer"],
+                          "department": "кафедра систем информатики",
+                          "diplomaTopics": [{
+                                "name": "Разработка модуля для системы оркестрации контейнеров",
+                               "summary": "надо че то сделать",
+                               "neededSkills": ["стрессоустойчивость"],
+                               "scientificField": "системы оркестрации контейнеров"
+                          }]
+                        }
+                        """)
                 .when()
                 .post("/profiles/mentors")
                 .then()
@@ -115,6 +116,28 @@ public class MentorIntegrationTest extends BaseIntegrationTest {
     }
     @Test
     public void checkValidationTgNickname() {
+        Response response = given().header("Content-Type", "application/json")
+                .and()
+                .body("""
+            {
+              "name": "Дмитрий Иртеров",
+              "email": "fatnsu.ru",
+              "tgNickname": "@FatBrother",
+              "scientificInterests": ["ОСИ", "peer-to-peer"],
+              "diplomaTopics": ["Системы оркестрации контейнеров бла бла бла"],
+              "department": "кафедра систем информатики"
+            }
+            """)
+                .when()
+                .post("/profiles/mentors")
+                .then()
+                .extract().response();
+
+        Assertions.assertEquals(200, response.statusCode());
+    }
+    @Test
+    public void checkValidationTgNickname2() {
+        // формат "@nickname"
         Response response = given().header("Content-Type", "application/json")
                 .and()
                 .body("""
@@ -135,7 +158,30 @@ public class MentorIntegrationTest extends BaseIntegrationTest {
         Assertions.assertNotEquals(200, response.statusCode());
     }
     @Test
+    public void checkValidationTgNickname3() {
+        // "-" запрещено
+        Response response = given().header("Content-Type", "application/json")
+                .and()
+                .body("""
+            {
+              "name": "Дмитрий Иртеров",
+              "email": "fatnsu.ru",
+              "tgNickname": "@fat-Brother",
+              "scientificInterests": ["ОСИ", "peer-to-peer"],
+              "diplomaTopics": ["Системы оркестрации контейнеров бла бла бла"],
+              "department": "кафедра систем информатики"
+            }
+            """)
+                .when()
+                .post("/profiles/mentors")
+                .then()
+                .extract().response();
+
+        Assertions.assertNotEquals(200, response.statusCode());
+    }
+    @Test
     public void checkValidationName() {
+        // Только буквы!
         Response response = given().header("Content-Type", "application/json")
                 .and()
                 .body("""
