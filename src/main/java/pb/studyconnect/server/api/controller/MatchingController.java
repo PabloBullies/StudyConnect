81b2a9f4c37e1cbb8bba1f55b594f1c5c0823863
Pechenuxa1 <v.tatarintsev@g.nsu.ru>
2024-11-22 23:24:07 +0700

    add get all matching students and mentors API

--- src/main/java/pb/studyconnect/server/api/controller/MatchingController.java
+++ src/main/java/pb/studyconnect/server/api/controller/MatchingController.java
@@ -1,12 +1,17 @@
 package pb.studyconnect.server.api.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pb.studyconnect.server.api.dto.response.MentorResponse;
+import pb.studyconnect.server.api.dto.response.StudentResponse;
 import pb.studyconnect.server.service.matchings.MatchingService;
+
+import java.util.List;
 
 import static pb.studyconnect.server.api.path.ApiPaths.MATCHES;
 import static pb.studyconnect.server.api.path.ApiPaths.MENTORS;
@@ -31,6 +36,16 @@ public class MatchingController {
             @RequestParam Boolean isApprove
     ) {
         matchingService.matchStudent(studentId, mentorId, isApprove);
+    }
+
+    @GetMapping("/{studentId}" + MENTORS)
+    public List<MentorResponse> getMatchingMentors(@PathVariable String studentId) {
+        return null;
+    }
+
+    @GetMapping("/{mentorId}" + STUDENTS)
+    public List<StudentResponse> getMatchingStudents(@PathVariable String mentorId) {
+        return null;
     }
 }
 
